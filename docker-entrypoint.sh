b94521c2a2745db5f7832693b3dd8bc618dc405a
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2020-08-18 08:04:42 -0400

    Fix terraform version print during entrypoint when not on latest

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [ $# -eq 0 ]; then
   # Print select tool versions then open an bash shell
   echo -en "aws-cli\t\t" && command aws --version | awk -F' ' '{print $1}' | awk -F'/' '{print $2}'
   echo -en "azure-cli\t" && command az version | jq -r '.["azure-cli"]'
-  echo -en "terraform\t" && command terraform version | awk -F' ' '{print $2}' | sed 's/^v//'
+  echo -en "terraform\t" && command terraform version | head -1 | awk -F' ' '{print $2}' | sed 's/^v//'
   echo -en "packer\t\t"  && command packer --version
   echo -en "ansible\t\t" && command ansible --version | head -1 | awk -F' ' '{print $2}'
 
