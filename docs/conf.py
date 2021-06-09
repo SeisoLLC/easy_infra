9645662ad9c72ec37c7267d69ad556e6c09eb48d
Sean Cavanaugh <sean.cavanaugh@seisollc.com>
2021-06-09 15:22:27 -0400

    Fixes to doc config files for readthedocs integration

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = "2021.06.01"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["m2r2"]
+extensions = ["m2r2", "sphinx_rtd_theme"]
 
 source_suffix = [".rst", ".md"]
 
@@ -48,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
