a7f2219d2173f19ab4d5218c6bef5f876d1c71cf
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-08-30 11:23:43 -0400

    Revert "Bump version: 2021.07.01 → 2021.08.01"
    This reverts commit e48cfcf91db7b0d299ecf1a915cce00cede82d93.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Seiso, LLC"
 author = "Jon Zeolla"
 
 # The full version, including alpha/beta/rc tags
-release = "2021.08.01"
+release = "2021.07.01"
 
 
 # -- General configuration ---------------------------------------------------
