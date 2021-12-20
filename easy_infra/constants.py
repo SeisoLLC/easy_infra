6afac0131b3f354d0c011e31e9bacfce3d6550a6
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-12-20 09:12:25 -0500

    Update primary components

--- easy_infra/constants.py
+++ easy_infra/constants.py
@@ -40,6 +40,6 @@ PYTHON_PACKAGES = {"checkov"}
 HASHICORP_PROJECTS = {"terraform", "packer"}
 # The following line is touchy, see easy_infra/util.py's
 # update_container_security_scanner function
-CONTAINER_SECURITY_SCANNER = "aquasec/trivy:0.21.2"
+CONTAINER_SECURITY_SCANNER = "aquasec/trivy:0.21.3"
 UNACCEPTABLE_VULNS = ["CRITICAL"]
 INFORMATIONAL_VULNS = ["UNKNOWN", "LOW", "MEDIUM", "HIGH"]
