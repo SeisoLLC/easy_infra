60c0d72aee28773e943aeff55e7b0a2b33916096
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2022-03-31 15:56:59 -0400

    Baah

--- easy_infra/constants.py
+++ easy_infra/constants.py
@@ -3,10 +3,10 @@ easy_infra constants
 """
 
 import json
-import git
-from typing import Union
 from pathlib import Path
+from typing import Union
 
+import git
 from easy_infra import __project_name__, __version__, utils
 
 CONFIG_FILE = Path(f"{__project_name__}.yml").absolute()
