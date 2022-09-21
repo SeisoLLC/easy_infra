ff7dcd3679ebe1016fefd65f78cc9a1074709eae
Seiso Automation <automation@seisollc.com>
2022-09-21 01:32:55 +0000

    Bump version: 2022.09.10 → 2022.09.11

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ copyright = "2022, Seiso, LLC"
 author = "Jon Zeolla"
 
 # The full version, including alpha/beta/rc tags
-release = "2022.09.10"
+release = "2022.09.11"
 
 
 # -- General configuration ---------------------------------------------------
