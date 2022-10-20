6388ccbec1a88fe55a62bbaf15fd4f49e49c945e
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2022-10-20 10:43:25 -0400

    Add logging into the default hooks

--- hooks/40-set_default_terraform_version.sh
+++ hooks/40-set_default_terraform_version.sh
@@ -7,23 +7,24 @@ source /usr/local/bin/common.sh
 
 
 current_version=$(cat /home/easy_infra/.tfenv/version)
+output_log="/var/log/$(basename "${BASH_SOURCE[0]}")"
 
 if [[ "${TERRAFORM_VERSION}" == "${current_version}" ]]; then
   exit 0
 fi
 
 # TERRAFORM_VERSION is not the installed and active version; make it so
-tfenv install "${TERRAFORM_VERSION}" &>/dev/null
+tfenv install "${TERRAFORM_VERSION}" &>>"${output_log}"
 return=$?
 if [[ "${return}" != 0 ]]; then
   message="Unable to install Terraform ${TERRAFORM_VERSION}, continuing to use version ${current_version}..."
-  _feedback WARNING "${message}"
+  _feedback WARNING "${message}" | tee -a "${output_log}"
   fully_qualified_script=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
   # shellcheck disable=SC2154 # dir is exported in the parent process, see functions.j2
   _log "easy_infra.hook" info failure "${fully_qualified_script}" "${dir}" string "${message}"
   exit 0
 fi
 
-_feedback INFORMATIONAL "Switching Terraform version to ${TERRAFORM_VERSION} due to the provided TERRAFORM_VERSION environment variable..."
-tfenv use "${TERRAFORM_VERSION}" &>/dev/null
+_feedback INFORMATIONAL "Switching Terraform version to ${TERRAFORM_VERSION} due to the provided TERRAFORM_VERSION environment variable..." | tee -a "${output_log}"
+tfenv use "${TERRAFORM_VERSION}" &>>"${output_log}"
 exit $?
--- hooks/50-terraform_dynamic_version_use.sh
+++ hooks/50-terraform_dynamic_version_use.sh
@@ -6,6 +6,7 @@
 source /usr/local/bin/common.sh
 
 fully_qualified_script=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+output_log="/var/log/$(basename "${BASH_SOURCE[0]}")"
 
 # The rest of this script is not relevant if AUTODETECT is not set to true
 if [[ "${AUTODETECT}" != "true" ]]; then
@@ -15,26 +16,26 @@ fi
 current_version=$(cat /home/easy_infra/.tfenv/version)
 
 # Use tfenv to get the minimum version of terraform specified in the terraform code (in the current directory)
-terraform_min_required="$(tfenv min-required 2>/dev/null)"
+terraform_min_required="$(tfenv min-required 2>>"${output_log}")"
 
 if [[ ! "${terraform_min_required}" ]]; then
   message="Unable to detect the minimum required version of Terraform, falling back to version ${TERRAFORM_VERSION}"
-  _feedback WARNING "${message}"
+  _feedback WARNING "${message}" | tee -a "${output_log}"
   # shellcheck disable=SC2154 # dir is exported in the parent process, see functions.j2
   _log "easy_infra.hook" info failure "${fully_qualified_script}" "${dir}" string "${message}"
-  tfenv use "${TERRAFORM_VERSION}" &>/dev/null
+  tfenv use "${TERRAFORM_VERSION}" &>>"${output_log}"
   exit $?
 fi
 
 if [[ "${TERRAFORM_VERSION}" == "${terraform_min_required}" && "${TERRAFORM_VERSION}" == "${current_version}" ]]; then
-  _feedback INFORMATIONAL "The detected minimum Terraform version of ${terraform_min_required} is already installed and in use"
+  _feedback INFORMATIONAL "The detected minimum Terraform version of ${terraform_min_required} is already installed and in use" | tee -a "${output_log}"
   exit 0
 fi
 
 ## Check if the detected required version of terraform is already installed, and if so, use it
 if [[ "${TERRAFORM_VERSION}" == "${terraform_min_required}" ]]; then
-  _feedback INFORMATIONAL "The detected minimum Terraform version of ${terraform_min_required} is already installed; ensuring it's in use..."
-  tfenv use "${TERRAFORM_VERSION}" &>/dev/null
+  _feedback INFORMATIONAL "The detected minimum Terraform version of ${terraform_min_required} is already installed; ensuring it's in use..." | tee -a "${output_log}"
+  tfenv use "${TERRAFORM_VERSION}" &>>"${output_log}"
   exit $?
 fi
 
@@ -43,13 +44,13 @@ tfenv install "${terraform_min_required}" &>/dev/null
 return=$?
 if [[ "${return}" != 0 ]]; then
   message="Unable to install Terraform ${terraform_min_required}, falling back to version ${TERRAFORM_VERSION}..."
-  _feedback WARNING "${message}"
+  _feedback WARNING "${message}" | tee -a "${output_log}"
   # shellcheck disable=SC2154 # dir is exported in the parent process, see functions.j2
   _log "easy_infra.hook" info failure "${fully_qualified_script}" "${dir}" string "${message}"
   tfenv use "${TERRAFORM_VERSION}" &>/dev/null
   exit $?
 fi
 
-_feedback INFORMATIONAL "Switching Terraform version to ${terraform_min_required}..."
-tfenv use "${terraform_min_required}" &>/dev/null
+_feedback INFORMATIONAL "Switching Terraform version to ${terraform_min_required}..." | tee -a "${output_log}"
+tfenv use "${terraform_min_required}" &>>"${output_log}"
 exit $?
