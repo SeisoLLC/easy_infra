86b745ae9f493f7df78468ecce3f5c772fe8d571
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2022-12-29 15:13:14 -0500

    Automate test maintenance when updating components

--- easy_infra/config.py
+++ easy_infra/config.py
@@ -45,29 +45,29 @@ def write_config(*, config: dict, config_file: Path):
         dump(config, file)
 
 
-def update_config_file(*, thing: str, version: str):
+def update_config_file(*, package: str, version: str):
     """Update the easy_infra config file"""
     # Normalize
-    thing = thing.split("/")[-1].lower()
+    package = package.split("/")[-1].lower()
     if isinstance(version, bytes):
         version = version.decode("utf-8").rstrip()
 
     config_file = Path(f"{__project_name__}.yml").absolute()
 
     config = parse_config(config_file=config_file)
-    allow_update = config["packages"][thing].get("allow_update", True)
-    current_version = config["packages"][thing]["version"]
+    allow_update = config["packages"][package].get("allow_update", True)
+    current_version = config["packages"][package]["version"]
 
     if version == current_version:
-        LOG.debug(f"No new versions have been detected for {thing}")
+        LOG.debug(f"No new versions have been detected for {package}")
         return
 
     if not allow_update:
         LOG.warning(
-            f"Not updating {thing} to version {version} because allow_update is set to false"
+            f"Not updating {package} to version {version} because allow_update is set to false"
         )
         return
 
-    config["packages"][thing]["version"] = version
-    LOG.info(f"Updating {thing} to version {version}")
+    config["packages"][package]["version"] = version
+    LOG.info(f"Updating {package} to version {version}")
     write_config(config=config, config_file=config_file)
