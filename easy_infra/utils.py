54d95cebdafc361cca20bc1eadca7bc0b9da74be
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2023-01-30 10:26:55 -0500

    Appease the goat; break up long log messages

--- easy_infra/utils.py
+++ easy_infra/utils.py
@@ -119,7 +119,8 @@ def opinionated_docker_run(
         container.remove()
         if not is_status_expected(expected=expected_exit, response=response):
             LOG.error(
-                f'Received an exit code of {response["StatusCode"]} when {expected_exit} was expected when invoking CLIENT.containers.run() with the following arguments: '
+                f'Received an exit code of {response["StatusCode"]} when {expected_exit} was expected'
+                + " when invoking CLIENT.containers.run() with the following arguments: "
                 + f"{auto_remove=}, {command=}, {detach=}, {environment=}, {image=}, {network_mode=}, {tty=}, {volumes=}, {working_dir=}"
             )
 
