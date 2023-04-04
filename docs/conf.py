d716ce1fe8c1c71d75d087bb87269f8d76cf5659
Seiso Automation <automation@seisollc.com>
2023-04-04 09:51:18 +0000

    Bump version: 2023.03.07 → 2023.04.01

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ copyright = "2022, Seiso, LLC"
 author = "Jon Zeolla"
 
 # The full version, including alpha/beta/rc tags
-release = "2023.03.07"
+release = "2023.04.01"
 
 
 # -- General configuration ---------------------------------------------------
--- easy_infra/__init__.py
+++ easy_infra/__init__.py
@@ -4,4 +4,4 @@ easy_infra init
 __maintainer__ = "Seiso"
 __copyright__ = "(c) 2022 Seiso, LLC"
 __project_name__ = "easy_infra"
-__version__ = "2023.03.07"
+__version__ = "2023.04.01"
