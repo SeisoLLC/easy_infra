a27d1306d728e1b6cacf6959a7b91dd7ab530eeb
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2024-01-05 13:42:04 -0500

    chore: move to tofuenv for opentofu

--- build/hooks/50-opentofu_dynamic_version_use.sh
+++ build/hooks/50-opentofu_dynamic_version_use.sh
@@ -13,17 +13,17 @@ if [[ "${AUTODETECT}" != "true" ]]; then
   exit 0
 fi
 
-current_version=$(cat /home/easy_infra/.tfenv/version)
+current_version=$(cat /home/easy_infra/.tofuenv/version)
 
-# Use tfenv to get the minimum version of opentofu specified in the opentofu code (in the current directory)
-opentofu_min_required="$(tfenv min-required 2>>"${output_log}")"
+# Use tofuenv to get the minimum version of opentofu specified in the opentofu code (in the current directory)
+opentofu_min_required="$(tofuenv min-required 2>>"${output_log}")"
 
 if [[ ! "${opentofu_min_required}" ]]; then
   message="Unable to detect the minimum required version of OpenTofu, falling back to version ${OPENTOFU_VERSION}"
   _feedback WARNING "${message}" | tee -a "${output_log}"
   # shellcheck disable=SC2154 # dir is exported in the parent process, see functions.j2
   _log "easy_infra.hook" info failure "${fully_qualified_script}" "${dir}" string "${message}"
-  tfenv use "${OPENTOFU_VERSION}" &>>"${output_log}"
+  tofuenv use "${OPENTOFU_VERSION}" &>>"${output_log}"
   exit $?
 fi
 
@@ -35,22 +35,22 @@ fi
 ## Check if the detected required version of opentofu is already installed, and if so, use it
 if [[ "${OPENTOFU_VERSION}" == "${opentofu_min_required}" ]]; then
   _feedback INFO "The detected minimum OpenTofu version of ${opentofu_min_required} is already installed; ensuring it's in use..." | tee -a "${output_log}"
-  tfenv use "${OPENTOFU_VERSION}" &>>"${output_log}"
+  tofuenv use "${OPENTOFU_VERSION}" &>>"${output_log}"
   exit $?
 fi
 
 ## Change versions to the detected min_required
-tfenv install "${opentofu_min_required}" &>/dev/null
+tofuenv install "${opentofu_min_required}" &>/dev/null
 return=$?
 if [[ "${return}" != 0 ]]; then
   message="Unable to install OpenTofu ${opentofu_min_required}, falling back to version ${OPENTOFU_VERSION}..."
   _feedback WARNING "${message}" | tee -a "${output_log}"
   # shellcheck disable=SC2154 # dir is exported in the parent process, see functions.j2
   _log "easy_infra.hook" info failure "${fully_qualified_script}" "${dir}" string "${message}"
-  tfenv use "${OPENTOFU_VERSION}" &>/dev/null
+  tofuenv use "${OPENTOFU_VERSION}" &>/dev/null
   exit $?
 fi
 
 _feedback INFO "Switching OpenTofu version to ${opentofu_min_required}..." | tee -a "${output_log}"
-tfenv use "${opentofu_min_required}" &>>"${output_log}"
+tofuenv use "${opentofu_min_required}" &>>"${output_log}"
 exit $?
