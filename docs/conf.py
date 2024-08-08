72c00ca6f2623f7b5d5b3c19672c97cd78c73f59
Seiso Automation <automation@seisollc.com>
2024-08-08 03:36:57 +0000

    Bump version: 2024.07.03 → 2024.08.01

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ copyright = "2022, Seiso, LLC"
 author = "Jon Zeolla"
 
 # The full version, including alpha/beta/rc tags
-release = "2024.07.03"
+release = "2024.08.01"
 
 
 # -- General configuration ---------------------------------------------------
